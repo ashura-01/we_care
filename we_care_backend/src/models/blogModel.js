6c086420301fe2f4ebfec7b285020f2ae212be40
ashura <fmn.flame@gmail.com>
2026-03-29 03:02:55 +0600

    blog comment model and controller design and aggregation with blogs

--- we_care_backend/src/models/blogModel.js
+++ we_care_backend/src/models/blogModel.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const DataSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    category: { type: String, required: true }, 
+    category: { type: String, required: true },
     image: { type: String, required: true },
     description: { type: String, required: true },
     shortDescription: { type: String, required: true },
-    
-  
+
+
     authorId: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
   },
   {
